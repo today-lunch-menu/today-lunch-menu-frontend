6e28622b7fc5afc0c726d455f8ddc61676e63917
조성원 <tjddnjsjo@kakao.com>
2023-10-28 23:44:03 +0900

    design: 행간 추가

--- src/components/Recommend.tsx
+++ src/components/Recommend.tsx
@@ -10,14 +10,15 @@ export default function Recommend({ reason, menu }: RecommendProps) {
   return (
     <Section>
       <div className="flex flex-col gap-8 items-center mx-auto max-w-xl text-center">
-        <h1 className="text-2xl font-bold sm:text-4xl">
-          오늘 점심 {reason}
-          <br className="my-4" />
-          <strong className="text-4xl font-extrabold sm:text-6xl">
-            "{menu}"
-          </strong>{' '}
-          어때요?
-        </h1>
+        <div>
+          <h1 className="text-2xl font-bold sm:text-4xl">오늘 점심 {reason}</h1>
+          <h1 className="mt-4 text-2xl font-bold sm:text-4xl">
+            <strong className="text-4xl font-extrabold sm:text-6xl">
+              "{menu}"
+            </strong>{' '}
+            어때요?
+          </h1>
+        </div>
 
         <img src="http://placehold.it/500x350" alt={menu} />
 
