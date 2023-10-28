1e55b15f8472d376b1aa3e7010d64fc9c0247345
조성원 <tjddnjsjo@kakao.com>
2023-10-28 23:35:21 +0900

    feat: Header, Recommend 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import Recommend from './components/Recommend';
+import Header from './components/ui/Header';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header
+        title="오늘 점심 뭐 먹을래?"
+        description="경아님이 점심 메뉴를 추천해드립니다! 🎉"
+      />
+      <Recommend reason="비오니까" menu="돼지 국밥" />
+    </>
+  );
 }
 
 export default App;
