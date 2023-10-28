e76055c203898e0188cb0a9599fae94a7d918504
조성원 <tjddnjsjo@kakao.com>
2023-10-28 23:34:10 +0900

    refactor(Header): Type 추가

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,4 +1,10 @@
-export default function Header({ title, description, children }) {
+type HeaderProps = {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+};
+
+export default function Header({ title, description, children }: HeaderProps) {
   return (
     <header>
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
