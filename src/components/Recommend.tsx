a7bfa902960e842f2cd688ae1e49489ba11f0271
조성원 <tjddnjsjo@kakao.com>
2023-10-28 23:35:13 +0900

    feat: 메뉴 추천 섹션 구현

---
+++ src/components/Recommend.tsx
@@ -0,0 +1,31 @@
+import Button from './ui/Button';
+import Section from './ui/Section';
+
+type RecommendProps = {
+  reason: string;
+  menu: string;
+};
+
+export default function Recommend({ reason, menu }: RecommendProps) {
+  return (
+    <Section>
+      <div className="flex flex-col gap-8 items-center mx-auto max-w-xl text-center">
+        <h1 className="text-2xl font-bold sm:text-4xl">
+          오늘 점심 {reason}
+          <br className="my-4" />
+          <strong className="text-4xl font-extrabold sm:text-6xl">
+            "{menu}"
+          </strong>{' '}
+          어때요?
+        </h1>
+
+        <img src="http://placehold.it/500x350" alt={menu} />
+
+        <div className="flex flex-wrap justify-center gap-4">
+          <Button title="좋아요!" color="blue" />
+          <Button title="다른거 먹을래요..." color="transparent" />
+        </div>
+      </div>
+    </Section>
+  );
+}
