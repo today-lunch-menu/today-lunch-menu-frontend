f90f85e1be5cb861f48f4edc5120dc66dfce5fde
조성원 <tjddnjsjo@kakao.com>
2023-10-30 02:39:22 +0900

    refactor: 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import Header from './components/ui/Header';
+import Navbar from './components/Navbar';
 import RecommendMenu from './components/RecommendMenu';
 import FindRestaurants from './components/FindRestaurants';
 
-function App() {
+export default function App() {
   return (
     <>
-      <Header
-        title="오늘 점심 뭐 먹을래?"
-        description="경아님이 점심 메뉴를 추천해드립니다! 🎉"
-      >
-        Button?
-      </Header>
-      <RecommendMenu reason="비오니까" menu="돼지 국밥" />
-      <FindRestaurants menu="돼지 국밥" />
+      <Navbar />
+      <RecommendMenu reason="비오니까" menu="돼지국밥" />
+      <FindRestaurants menu="돼지국밥" />
     </>
   );
 }
-
-export default App;
