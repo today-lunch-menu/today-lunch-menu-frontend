c8b0194f49b262a988ddbd5e833b7a4130fa24c4
조성원 <tjddnjsjo@kakao.com>
2023-10-29 23:25:18 +0900

    feat: Section index에 따른 배경 색 구현

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -1,11 +1,12 @@
 type SectionProps = {
+  index: number;
   children: React.ReactNode;
 };
 
-export default function Section({ children }: SectionProps) {
+export default function Section({ index, children }: SectionProps) {
   return (
-    <section className="bg-gray-50">
-      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
+    <section className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
+      <div className="mx-auto max-w-screen-xl px-4 py-20 lg:flex lg:items-center">
         {children}
       </div>
     </section>
