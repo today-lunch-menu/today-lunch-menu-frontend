4d8b4f7b4b8bdad9c037ddff2cc066f1bd185b24
조성원 <tjddnjsjo@kakao.com>
2023-10-29 23:26:46 +0900

    feat: 카카오 지도 추가

---
+++ src/components/KakaoMap.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+export default function KakaoMap() {
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new window.kakao.maps.Map(container, options);
+  }, []);
+
+  return (
+    <div
+      id="map"
+      style={{
+        width: '500px',
+        height: '500px',
+      }}
+    />
+  );
+}
