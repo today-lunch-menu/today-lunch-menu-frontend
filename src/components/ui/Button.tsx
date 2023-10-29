eb2adeb09b16c41f81bfd5d2c12841fe4e5d2590
조성원 <tjddnjsjo@kakao.com>
2023-10-30 02:32:28 +0900

    fix: 버튼 색상 오류 수정

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,25 @@
 type ButtonProps = {
   title: string;
   color: string;
+  disabled?: boolean;
 };
 
-export default function Button({ title, color }: ButtonProps) {
-  const classes = `block w-full rounded bg-${color}-600 px-12 py-3 text-sm font-medium shadow hover:bg-${color}-700 focus:outline-none focus:ring active:bg-${color}-500 sm:w-auto`;
+export default function Button({
+  title,
+  color,
+  disabled = false,
+}: ButtonProps) {
+  const colorVariants: { [key: string]: string } = {
+    blue: 'bg-blue-600 hover:bg-blue-500 text-white active:bg-blue-700',
+    red: 'bg-red-500 hover:bg-red-400 text-white active:bg-red-700',
+    yellow: 'bg-yellow-300 hover:bg-yellow-400 text-black active:bg-yellow-500',
+  };
 
-  return <button className={classes}>{title}</button>;
+  const classes = ` ${colorVariants[color]} block w-full rounded px-12 py-3 text-sm font-medium shadow focus:outline-none focus:ring sm:w-auto`;
+
+  return (
+    <button className={classes} disabled={disabled}>
+      {title}
+    </button>
+  );
 }
