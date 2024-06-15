4e28ab83bd25ff1380a26964280a388a88a44166
partykyoung <partykyoung@gmail.com>
2024-06-16 06:59:10 +0900

    wip: 음식 정리 중

--- src/data/culinary-culture.ts
+++
@@ -1,8 +0,0 @@
-export const culinaryCulture = {
-  koreanCuisine: '한식',
-  chineseCuisine: '일식',
-  japaneseCuisine: '중식',
-  westernCuisine: '양식',
-  asianCuisine: '아시아 음식',
-  dessert: '디저트'
-}
--- src/data/food.ts
+++ src/data/food.ts
@@ -1,4 +1,4 @@
-import { culinaryCulture } from './culinary-culture';
+import { korean, western, japanese, chinese, asian } from './culture-food';
 import { emotion } from './emotion';
 import { weather } from './weather';
 
@@ -13,16 +13,50 @@ const foodKeys = (Object.keys(weather) as WeatherKeys[]).reduce(
   {} as WeatherKeyMap,
 );
 
-export const food: Record<WeatherKeys, any[]> = {
-  [foodKeys.sunny]: [],
-  [foodKeys.partlyCloudy]: [],
-  [foodKeys.mostlyCloudy]: [],
-  [foodKeys.cloudy]: [],
-  [foodKeys.rain]: [],
-  [foodKeys.snow]: [],
-  [foodKeys.rainOrSnow]: [],
-  [foodKeys.snowOrRain]: [],
-  [foodKeys.thunderstorm]: [],
-  [foodKeys.fog]: [],
-  [foodKeys.yellowDust]: [],
+export const food = {
+  [foodKeys.sunny]: [
+    korean['돼지국밥'],
+    korean['순대국밥'],
+    korean['전주비빔밥'],
+    korean['소머리국밥'],
+    korean['콩나물국밥'],
+    korean['떡볶이'],
+    korean['육개장'],
+    korean['닭개장'],
+    korean['참치김밥'],
+    korean['제육볶음'],
+    korean['갈비찜'],
+    korean['바지락칼국수'],
+    korean['해물칼국수'],
+    korean['콩국수'],
+    korean['냉면'],
+    korean['고등어조림'],
+    korean['알탕'],
+    asian['목살 필라프'],
+    asian['새우 필라프'],
+  ],
+  [foodKeys.partlyCloudy]: [korean['돼지국밥'], korean['순대국밥']],
+  [foodKeys.mostlyCloudy]: [korean['돼지국밥'], korean['순대국밥']],
+  [foodKeys.cloudy]: [korean['돼지국밥'], korean['순대국밥']],
+  [foodKeys.rain]: [
+    korean['돼지국밥'],
+    korean['순대국밥'],
+    korean['소머리국밥'],
+    korean['굴국밥'],
+    korean['김치전'],
+    korean['해물파전'],
+    korean['동래파전'],
+    korean['곱창전골'],
+    korean['버섯전골'],
+  ],
+  [foodKeys.snow]: [korean['돼지국밥'], korean['순대국밥'], korean['추어탕']],
+  [foodKeys.thunderstorm]: [
+    korean['돼지국밥'],
+    korean['순대국밥'],
+    korean['비빔밥'],
+    korean['한식 카레'],
+    korean['김밥'],
+  ],
+  [foodKeys.fog]: [korean['돼지국밥'], korean['순대국밥']],
+  [foodKeys.yellowDust]: [korean['돼지국밥'], korean['순대국밥']],
 };
